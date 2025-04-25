17f3de040d2d00a629f5825a10cf42d8aa0820b7
ZeyadMohamed <zeyadelsharawy709@gmail.com>
2025-04-25 03:39:32 +0300

    Add token handling in Google API route to store token in cookies and redirect to homepage. This enhances session management and user experience.

--- src/app/api/google/route.ts
+++ src/app/api/google/route.ts
@@ -1,4 +1,27 @@
 import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export async function GET(request: Request) {
+  // Extract token from search params
+  const url = new URL(request.url);
+  const token = url.searchParams.get("token");
+  console.log("Token from search params:", token);
+
+  // Save token in cookies if it exists
+  if (token) {
+    // Store the token in cookies
+    const cookieStore = await cookies();
+    cookieStore.set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 60 * 24 * 7, // 1 week
+      path: "/",
+    });
+  }
+
+  // Redirect to homepage or dashboard after setting the cookie
+  return NextResponse.redirect(new URL("/", request.url));
+}
 
 export async function POST(request: Request) {
   // Extract search params from the URL
