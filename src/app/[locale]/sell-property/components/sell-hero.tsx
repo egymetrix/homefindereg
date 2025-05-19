0bf48ad7776730832088769a4746804f6b0b3c92
ZeyadMohamed <zeyadelsharawy709@gmail.com>
2025-05-19 14:03:59 +0300

    Enhance Arabic localization by adding new sell-form messages for improved user interaction. Refactor SellHero component for better responsiveness and cleaner design, adjusting text sizes and margins for a more polished appearance.

--- src/app/[locale]/sell-property/components/sell-hero.tsx
+++ src/app/[locale]/sell-property/components/sell-hero.tsx
@@ -1,12 +1,13 @@
 import Banner from "@/components/shared/Banner";
 import { useTranslations } from "next-intl";
+
 const SellHero = () => {
   const t = useTranslations("sell-hero");
   return (
     <Banner backgroundImage="/images/hero-sell-estate.jpg" height="70vh">
-      <div className="flex flex-col items-start justify-center text-white max-w-4xl ml-10">
-        <h1 className="text-7xl font-bold">{t("title")}</h1>
-        <p className="text-2xl">{t("description")}</p>
+      <div className="flex flex-col items-start justify-center text-white max-w-4xl ml-4 md:ml-10">
+        <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold">{t("title")}</h1>
+        <p className="text-lg md:text-xl lg:text-2xl mt-2">{t("description")}</p>
       </div>
     </Banner>
   );
